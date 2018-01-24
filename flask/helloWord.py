d39cda1989cf8d458ba22b973bf7064354558a46
jimmybasketball <867745834@qq.com>
2018-01-24 16:04:44 +0800

    flask_webform_flash

--- flask/helloWord.py
+++ flask/helloWord.py
@@ -1,6 +1,6 @@
 #!D:\Python\python27
 # -*- coding:utf-8 -*-
-from flask import Flask, render_template, redirect, session, url_for
+from flask import Flask, render_template, redirect, session, url_for,flash
 from flask_script import Manager
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
@@ -17,9 +17,12 @@ app.config['SECRET_KEY'] = 'hard to guess string'
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    name = None
+
     form = NameForm()
     if form.validate_on_submit():
+        old_name = session.get('name')
+        if old_name is not None and old_name != form.name.data:
+            flash('Looks like you have changed your name!')
         session['name'] = form.name.data
         return redirect(url_for('index'))
     return render_template('index.html', form=form, name=session.get('name'))
